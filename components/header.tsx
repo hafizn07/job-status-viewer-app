b72ab37ce1b5e98c024e214909cfc6d6335c7513
Hafiz N <hafiznjh007@gmail.com>
2024-11-07 17:00:57 +0400

    style: fix responsive style

--- components/header.tsx
+++ components/header.tsx
@@ -6,8 +6,10 @@ interface HeaderProps {
 }
 
 const Header = ({ statusFilter, setStatusFilter }: HeaderProps) => (
-  <div className="flex justify-between items-center mb-4">
-    <h1 className="text-2xl font-bold">Print Job Status Viewer</h1>
+  <div className="flex flex-col sm:flex-row justify-between items-center mb-4 gap-2 sm:gap-0">
+    <h1 className="text-xl sm:text-2xl font-bold text-center sm:text-left">
+      Print Job Status Viewer
+    </h1>
     <SelectFilter value={statusFilter} onChange={setStatusFilter} />
   </div>
 );
